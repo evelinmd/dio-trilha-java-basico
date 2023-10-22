8efbabea17e4ca8eb89ebbbf947ae5b83907ef4f
Evelin Dantas <evelin_dantas@hotmail.com>
2023-10-21 23:46:08 -0300

    Projeto Conta Banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            
+            System.out.println("Por favor, digite seu nome");
+            String nome = scanner.next();
+            
+            System.out.println("Por favor, digite a sua agencia");
+            String agencia = scanner.next();
+            
+            System.out.println("Por favor, digite sua conta");
+            int conta = scanner.nextInt();
+
+            double saldo = 234.00;
+
+            System.out.println("Olá " + nome + " Obrigado por criar uma conta em nosso banco!");
+            System.out.println("Sua agência é " + agencia + " e o número da sua conta é " + conta);
+            System.out.println ("Seu saldo atual é " + saldo + " e já está disponível para saque. ");
+
+
+        }
+        
     }
 }
